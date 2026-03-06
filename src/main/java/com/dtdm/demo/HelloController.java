313ff297b0d08f535b6cb5a46a6e355c818dde0f
kkitb004-afk <kkitb004@gmail.com>
2026-03-06 09:16:55 +0700

    springboot railway demo

---
+++ src/main/java/com/dtdm/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.dtdm.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "650+ Toeic thì đổi tên";
+    }
+
+}
